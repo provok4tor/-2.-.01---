f8b84b576f8155887766afaccddc4f578f139481
provok4tor <103079724+provok4tor@users.noreply.github.com>
2025-05-29 01:13:05 +0300

    Update BankTest.java

--- core/src/test/java/com/bank/BankTest.java
+++ core/src/test/java/com/bank/BankTest.java
@@ -3,15 +3,14 @@ package test.java.com.bank;
 import main.java.com.bank.Bank;
 import main.java.com.bank.BankAccount;
 import main.java.com.bank.Customer;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
 
-import org.mockito.Mockito;
-import org.mockito.Mockito.*;
-
+import java.util.ArrayList;
 import java.util.List;
 
 
