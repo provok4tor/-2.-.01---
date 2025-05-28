0d4ef5cc5a5649a8dd8e74e0ea70a528d07f05b7
provok4tor <103079724+provok4tor@users.noreply.github.com>
2025-05-28 21:37:17 +0300

    Update Customer.java

--- core/src/main/java/com/bank/Customer.java
+++ core/src/main/java/com/bank/Customer.java
@@ -1,19 +1,45 @@
-package main.java.com.bank;
+package com.bank;
 
-public class Customer {
+/**
+ * Класс, представляющий клиента банка.
+ */
+public final class Customer {
+    /**
+     * Имя клиента.
+     */
     private final String name;
+
+    /**
+     * Идентификатор паспорта клиента.
+     */
     private final String passportId;
 
-    public Customer(String name, String passportId) {
-        this.name = name;
-        this.passportId = passportId;
+    /**
+     * Конструктор класса Customer.
+     *
+     * @param nameParam       Имя клиента.
+     * @param passportIdParam Идентификатор паспорта клиента.
+     */
+    public Customer(final String nameParam, final String passportIdParam) {
+        this.name = nameParam;
+        this.passportId = passportIdParam;
     }
 
-    public String getName() { 
-        return name; 
+    /**
+     * Возвращает имя клиента.
+     *
+     * @return Имя клиента.
+     */
+    public String getName() {
+        return name;
     }
-    
-    public String getPassportId() { 
-        return passportId; 
+
+    /**
+     * Возвращает идентификатор паспорта клиента.
+     *
+     * @return Идентификатор паспорта клиента.
+     */
+    public String getPassportId() {
+        return passportId;
     }
 }
