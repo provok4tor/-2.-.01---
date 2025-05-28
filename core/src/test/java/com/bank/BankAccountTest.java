7d52362b83fc069324ffbbdae8600645ad4c59fc
provok4tor <103079724+provok4tor@users.noreply.github.com>
2025-05-28 23:19:02 +0300

    Create BankAccountTest.java

---
+++ core/src/test/java/com/bank/BankAccountTest.java
@@ -0,0 +1,43 @@
+package test.java.com.bank;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountTest {
+    private BankAccount account;
+    private Customer customer;
+
+    @BeforeEach
+    public void setUp() {
+        customer = new Customer("John Doe", "123456789");
+        account = new BankAccount(customer, 1000.0);
+    }
+
+    @Test
+    public void testDeposit() {
+        account.deposit(500.0);
+        assertEquals(1500.0, account.getBalance());
+    }
+
+    @Test
+    public void testWithdraw() {
+        account.withdraw(200.0);
+        assertEquals(800.0, account.getBalance());
+    }
+
+    @Test
+    public void testGetAccountNumber() {
+        assertNotNull(account.getAccountNumber());
+    }
+
+    @Test
+    public void testGetBalance() {
+        assertEquals(1000.0, account.getBalance());
+    }
+
+    @Test
+    public void testGetOwner() {
+        assertEquals(customer, account.getOwner());
+    }
+}
