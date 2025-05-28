67c6e4938a4b4cb1c4733609b08c55c802176741
provok4tor <103079724+provok4tor@users.noreply.github.com>
2025-05-29 02:01:41 +0300

    Update CustomerTest.java

--- core/src/test/java/com/bank/CustomerTest.java
+++ core/src/test/java/com/bank/CustomerTest.java
@@ -1,7 +1,9 @@
 package test.java.com.bank;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import main.java.com.bank.Bank;
+import main.java.com.bank.BankAccount;
+import main.java.com.bank.Customer;
 import org.junit.jupiter.api.Test;
 
 public class CustomerTest {
